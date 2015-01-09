379fccb21d5ed1b7a8627a8e883d4875a69a1cfb
feefles <eyeung@seas.upenn.edu>
2015-01-08 18:10:38 -0800

    move to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150108060207) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "centers", force: :cascade do |t|
     t.integer  "center_id"
     t.string   "location"
