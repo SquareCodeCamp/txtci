2853c86298c37bec41862eac08c1e805f2672932
vickiniu <vicki.niu@gmail.com>
2015-01-10 08:45:45 -0800

    Loads JSON result for message queries by sponsor ID

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,7 @@ class ApiController < ApplicationController
 	#Returns a message given the sponsor ID
 	#GET /find/something
 	def getMessageFromSponsor 
-		@sponsorMessage = Message.where(sponsor_id: params[:sponsor_id])
+		@sponsorMessages = Message.where(sponsor_id: params[:sponsor_id])
+		render :json => @sponsorMessages
 	end
 end
